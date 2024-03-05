837eb3cc05f7490227666936109f1a335896f71a
tom-blk <tom.bleek@proton.me>
2024-03-05 16:41:48 +0100

    Started working on adding mouse responsiveness to the rocket

--- components/Rocket.Component.tsx
+++ components/Rocket.Component.tsx
@@ -1,35 +1,54 @@
 import React, { useRef } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import { useLoader, useFrame } from '@react-three/fiber';
-import { RigidBody } from '@react-three/rapier';
+import { RapierRigidBody, RigidBody, vec3 } from '@react-three/rapier';
 import { Trail } from '@react-three/drei';
+import { Vector2 } from 'three';
 
 const Rocket = () => {
 
     const rocketModel = useLoader(GLTFLoader, "/3d/rocket/rocket.gltf")
-    const rocket = useRef<any>(null!)
+    const rocket = useRef<RapierRigidBody>(null!)
 
-    const calculateImpulseOnAxisK = (iPointer: number, iRocket: number) => {
-        const impulse = (iPointer - iRocket)^2
-        if(iPointer > iRocket){
-            console.log(impulse)
-            return impulse
-        }else if(iPointer < iRocket){
-            return -impulse
-        }else{
-            return 0
+    const calculateImpulse = 
+        (rocketPosition: {x: number, y: number, z: number}, pointerPosition: Vector2) :
+        {x: number, y: number, z: 0} => {
+
+        const comparePositionsAndReturnRatio = (a: number, b: number, axis?: string): number => {
+                const diff = Math.abs(a - b);
+                console.log(diff, axis)
+                let impulse: number;
+
+                if(diff > 0.5){
+                    impulse = 50;
+                }else if(diff > 0.1){
+                    impulse = 10;
+                }else if(diff > 0){
+                    impulse = 3;
+                }else{
+                    impulse = 0;
+                }
+
+                return a > b ? impulse : -impulse;
+        }
+
+        return {
+            x: comparePositionsAndReturnRatio(pointerPosition.x, rocketPosition.x, "x"), 
+            y: comparePositionsAndReturnRatio(pointerPosition.y, rocketPosition.y, "y"), 
+            z: 0
         }
     }
 
+    let frames: number = 0;
+
     useFrame((state, delta) => {
+        frames ++;
+
+        if(frames % 10 === 0)
         if(rocket.current){
             if(state.pointer.x){
-                const rocketPosition = rocket.current.translation()
-                rocket.current.applyImpulse({
-                    x: calculateImpulseOnAxisK(state.pointer.x, rocketPosition.x),
-                    y: calculateImpulseOnAxisK(state.pointer.y, rocketPosition.y),
-                    z: 0
-                })
+                const rocketPosition = vec3(rocket.current.translation());
+                rocket.current.addForce(calculateImpulse(rocketPosition, state.pointer), true);
             }
         }
     })
