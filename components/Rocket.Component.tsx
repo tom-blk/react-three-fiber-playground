1d23e5b28e3e791a84b3e64f5ddb17f04431006f
tom-blk <tom.bleek@proton.me>
2024-03-06 11:57:32 +0100

    Added rocket flying solo implementation

--- components/Rocket.Component.tsx
+++ components/Rocket.Component.tsx
@@ -3,74 +3,67 @@ import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import { useLoader, useFrame } from '@react-three/fiber';
 import { RapierRigidBody, RigidBody, vec3 } from '@react-three/rapier';
 import { Trail } from '@react-three/drei';
-import { Vector2 } from 'three';
+import { Mesh, Vector2 } from 'three';
+
+const MOVEMENT_SPEED = 15;
 
 const Rocket = () => {
 
     const rocketModel = useLoader(GLTFLoader, "/3d/rocket/rocket.gltf")
-    const rocket = useRef<RapierRigidBody>(null!)
+    const rocketBody = useRef<RapierRigidBody>(null!)
+    const rocket = useRef<Mesh>(null!);
 
-    const calculateImpulse = 
-        (rocketPosition: {x: number, y: number, z: number}, pointerPosition: Vector2) :
-        {x: number, y: number, z: 0} => {
-
-        const comparePositionsAndReturnRatio = (a: number, b: number, axis?: string): number => {
-                const diff = Math.abs(a - b);
-                console.log(diff, axis)
-                let impulse: number;
-
-                if(diff > 0.5){
-                    impulse = 50;
-                }else if(diff > 0.1){
-                    impulse = 10;
-                }else if(diff > 0){
-                    impulse = 3;
-                }else{
-                    impulse = 0;
-                }
-
-                return a > b ? impulse : -impulse;
-        }
-
-        return {
-            x: comparePositionsAndReturnRatio(pointerPosition.x, rocketPosition.x, "x"), 
-            y: comparePositionsAndReturnRatio(pointerPosition.y, rocketPosition.y, "y"), 
-            z: 0
-        }
-    }
-
-    let frames: number = 0;
 
     useFrame((state, delta) => {
-        frames ++;
-
-        if(frames % 10 === 0)
-        if(rocket.current){
-            if(state.pointer.x){
-                const rocketPosition = vec3(rocket.current.translation());
-                rocket.current.addForce(calculateImpulse(rocketPosition, state.pointer), true);
-            }
+        const translation = rocketBody.current.translation();
+        if(
+            translation.x > 10 || 
+            translation.x < -10 ||
+            translation.y > 10 ||
+            translation.y < -10
+        ) {
+            rocket.current.rotation.z +=.02;
         }
+        const angle = rocket.current.rotation.z;
+
+        const impulseDirection = {
+            x: Math.sin(angle) * MOVEMENT_SPEED * delta,
+            y: Math.cos(angle) * MOVEMENT_SPEED * delta,
+            z: 0
+        }
+
+        rocketBody.current.applyImpulse(impulseDirection, true)
     })
 
+    const leftTurn = () => {
+        rocketBody.current.addTorque(vec3({x: 0,y: 0,z: 10}), true);
+    }
+
+    const rightTurn = () => {
+        rocketBody.current.addTorque(vec3({x: 0,y: 0,z: -10}), true);
+    }
+
+    const accelerate = () => {
+        rocketBody.current.addForce(vec3({x: 0,y: 0,z: 10}), true);
+    }
+
     return (
-        <Trail 
-        width={7}
-        color={'#FF4F00'}
-        length={1.5}
-        decay={.1}
-        local={false}
-        stride={0}
-        interval={2}
-        attenuation={(width) => width}
-        >
-            <RigidBody ref={rocket}>
-                <mesh>
-                    <primitive object={rocketModel.scene} />
-                </mesh>
-            </RigidBody>
-        </Trail>
-        
+            <Trail 
+            width={7}
+            color={'#FF4F00'}
+            length={1.5}
+            decay={.1}
+            local={false}
+            stride={0}
+            interval={2}
+            attenuation={(width) => width}
+            >
+                <RigidBody ref={rocketBody}>
+                    <mesh ref={rocket}>
+                        <primitive object={rocketModel.scene} />
+                    </mesh>
+                </RigidBody>
+            </Trail>
     )
 }
 
